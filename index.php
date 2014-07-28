a0fcffd0dd7dbf7151a4364dc7bfde19b25f5f68
enmanuel.cueva <enmanuel.cueva@kurtgeiger.com>
2014-07-28 16:38:56 +0100

    setting up basic structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<head>
+    <!--Include css-->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!--  Uncomment the code below to enable JQuery. But take into account that it is also possible to achieve the
+    same effect without JQuery, that is a useful Kata to practice pure JS -->
+    <!--  <script src="vendor/jquery/jquery/jquery-2.1.1.min.js"></script>-->
+</head>
+<body>
+<h1>Front page</h1>
+<div id="pop-up" class="popout-overlay hidden">
+    <div class="pop-up-content-wrapper">
+        <div id="col1" class="column">
+            <img id="main-img" src="" alt="" width="456" height="650">
+        </div>
+        <div id="col2" class="column">
+            <div id="caption">
+                <p>
+                    Lorem ipsum dolor sit amet, maiores ornare ac fermentum,
+                    imperdiet ut vivamus a, nam lectus at nunc.
+                </p>
+            </div>
+            <div id="product">
+                <img id="product-img" src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+    <!--Include js-->
+    <script src="assets/js/script.js"></script>
+</footer>
